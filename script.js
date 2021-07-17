59fddd228aef0d2f6cabe2ca9650256c6fad3515
gavara rakesh <grakh.02@gmail.com>
2021-07-17 22:53:55 +0530

    added files

---
+++ script.js
@@ -0,0 +1,70 @@
+function del1(){
+	if (document.getElementById('1').style.display==="none") {
+		document.getElementById('1').style.display="block";
+	}else{
+		document.getElementById('1').style.display="none";
+	}
+}
+function del2(){
+	if (document.getElementById('2').style.display==="none") {
+		document.getElementById('2').style.display="block";
+	}else{
+		document.getElementById('2').style.display="none";
+	}
+}
+function del3(){
+	if (document.getElementById('3').style.display==="none") {
+		document.getElementById('3').style.display="block";
+	}else{
+		document.getElementById('3').style.display="none";
+	}
+}
+function del4(){
+	if (document.getElementById('4').style.display==="none") {
+		document.getElementById('4').style.display="block";
+	}else{
+		document.getElementById('4').style.display="none";
+	}
+}
+function del5(){
+	if (document.getElementById('5').style.display==="none") {
+		document.getElementById('5').style.display="block";
+	}else{
+		document.getElementById('5').style.display="none";
+	}
+}
+function del6(){
+	if (document.getElementById('6').style.display==="none") {
+		document.getElementById('6').style.display="block";
+	}else{
+		document.getElementById('6').style.display="none";
+	}
+}
+function del7(){
+	if (document.getElementById('7').style.display==="none") {
+		document.getElementById('7').style.display="block";
+	}else{
+		document.getElementById('7').style.display="none";
+	}
+}
+function del8(){
+	if (document.getElementById('8').style.display==="none") {
+		document.getElementById('8').style.display="block";
+	}else{
+		document.getElementById('8').style.display="none";
+	}
+}
+function del9(){
+	if (document.getElementById('9').style.display==="none") {
+		document.getElementById('9').style.display="block";
+	}else{
+		document.getElementById('9').style.display="none";
+	}
+}
+function del10(){
+	if (document.getElementById('10').style.display==="none") {
+		document.getElementById('10').style.display="block";
+	}else{
+		document.getElementById('10').style.display="none";
+	}
+}
